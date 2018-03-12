7cce6bb31ab2cae692467b1362f67220f8b50a95
julGi <julien.gibeault@outlook.com>
2018-03-12 15:30:03 +0100

    Formulaires et champs contôlés React

--- react/OC1/memory2/src/App.js
+++
@@ -1,9 +0,0 @@
-import React, { Component } from 'react'
-function Demo({ cssClass, title }) {
-  return <h1 class={cssClass}>{title}</h1>
-}
-
-ReactDOM.render(
-  <Demo cssClass="brittle" title="Not so good" />,
-  document.getElementById('root')
-)
